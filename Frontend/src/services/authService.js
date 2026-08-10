2e7d8d5b965c349c1c197c71ae6efdaadb230cb5
Rohith Jayaram <rohithjayaram33@gmail.com>
2026-08-10 19:53:19 +0530

    Fix API URL

--- Frontend/src/services/authService.js
+++ Frontend/src/services/authService.js
@@ -1,22 +1,21 @@
-import axios from 'axios';
+import axios from "axios";
 
-const API_URL = "https://employee-api-rohith-c0b4e8bfhjgkh8cn.centralindia-01.azurewebsites.net/api/Auth";
+const API_URL =
+  "https://employee-api-rohith-c0b4e8bfhjgkh8cn.centralindia-01.azurewebsites.net/api/Auth";
 
 export const login = async (email, password) => {
-const response = await axios.post(`${API_URL}/login`, {
-email,
-password,
-});
-
-return response.data;
+  const response = await axios.post(`${API_URL}/login`, {
+    email,
+    password,
+  });
+  return response.data;
 };
 
 export const register = async (fullName, email, password) => {
-const response = await axios.post(`${API_URL}/register`, {
-fullName,
-email,
-password,
-});
-
-return response.data;
+  const response = await axios.post(`${API_URL}/register`, {
+    fullName,
+    email,
+    password,
+  });
+  return response.data;
 };
